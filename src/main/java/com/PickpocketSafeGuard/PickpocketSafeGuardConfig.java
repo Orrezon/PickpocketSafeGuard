f9e00eab8cfad243bb62c38b4256bbcd0da89f81
Orrezon <chrisjanuzik@gmail.com>
2023-04-21 09:04:56 -0700

    Refactors to PickpocketSafeGuard and adds config setting for hpThreshold

---
+++ src/main/java/com/PickpocketSafeGuard/PickpocketSafeGuardConfig.java
@@ -0,0 +1,19 @@
+package com.PickpocketSafeGuard;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("example")
+public interface PickpocketSafeGuardConfig extends Config
+{
+	@ConfigItem(
+		keyName = "hpThreshold",
+		name = "HP Threshold",
+		description = "The HP amount that the 'pickpocket' option will no longer show as left click for when at or below."
+	)
+	default int hpThreshold()
+	{
+		return 3;
+	}
+}
--- src/main/java/com/PickpocketSafeGuard/PickpocketSafeGuardPlugin.java
+++ src/main/java/com/PickpocketSafeGuard/PickpocketSafeGuardPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.PickpocketSafeGuard;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,26 +14,26 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Pickpocket Safe Guard"
 )
-public class ExamplePlugin extends Plugin
+public class PickpocketSafeGuardPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private PickpocketSafeGuardConfig config;
 
 	@Override
 	protected void startUp() throws Exception
 	{
-		log.info("Example started!");
+		log.info("Pickpocket Safe Guard started!");
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
-		log.info("Example stopped!");
+		log.info("Pickpocket Safe Guard stopped!");
 	}
 
 	@Subscribe
@@ -41,13 +41,14 @@ public class ExamplePlugin extends Plugin
 	{
 		if (gameStateChanged.getGameState() == GameState.LOGGED_IN)
 		{
-			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.greeting(), null);
+
+			client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "Example says " + config.hpThreshold(), null);
 		}
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	PickpocketSafeGuardConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(PickpocketSafeGuardConfig.class);
 	}
 }
